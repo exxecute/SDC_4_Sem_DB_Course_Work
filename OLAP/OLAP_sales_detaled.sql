15d11dea7cb638ff314fd1cc0696c36a14b281ec
Uladzislau Mikhayevich <uladzislau.mikhayevich@student.ehu.lt>
2025-06-15 19:03:00 +0300

    main: querries in other scripts

--- OLAP/OLAP_sales_detaled.sql
+++ OLAP/OLAP_sales_detaled.sql
@@ -32,20 +32,4 @@ JOIN DimItem di ON fs.item_key = di.item_key
 JOIN ItemProductBridge ipb ON di.item_key = ipb.item_key
 JOIN DimProduct dp ON ipb.product_key = dp.product_key;
 
--- current inventory
-CREATE VIEW vw_inventory_current AS
-SELECT
-    fi.fact_inventory_key,
-    fi.item_key,
-    di.serial_number,
-    ipb.product_key,
-    dp.product_name,
-    dp.model_number,
-    dp.category_name,
-    dp.price,
-    fi.address
-FROM FactInventory fi
-JOIN DimItem di ON fi.item_key = di.item_key
-JOIN ItemProductBridge ipb ON di.item_key = ipb.item_key
-JOIN DimProduct dp ON ipb.product_key = dp.product_key
-WHERE dp.is_current = TRUE;
+SELECT * FROM vw_sales_detailed;
